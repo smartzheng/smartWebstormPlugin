20edd820abf1a3772efa3e062af985f7782422ac
GitHub Action <action@github.com>
2021-09-29 06:13:45 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/smartzheng/smartwebstormplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.smartzheng.smartwebstormplugin.services
+
+import com.github.smartzheng.smartwebstormplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/smartzheng/smartwebstormplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/smartzheng/smartwebstormplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.smartzheng.smartwebstormplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.smartzheng.smartwebstormplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
